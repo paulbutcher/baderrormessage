06c83c20f2db260d2a056e2c0a452b1e223ddace
Paul Butcher <paul@paulbutcher.com>
2011-10-09 16:35:33 +0100

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,1 @@
+crossScalaVersions := Seq("2.9.0", "2.9.1")
---
+++ src/main/scala/Foo.scala
@@ -0,0 +1,6 @@
+package com.example
+
+class Foo extends Function2[Int, Int, Int] {
+  def apply(x: Int, y: Int) = x + y
+  override def tupled: (Int, Int) => Int = super.tupled
+}
